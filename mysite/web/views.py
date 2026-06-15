8d47baf1a3a151d8271f72d354da249826dd1753
Nesar <nhasa0011@gmail.com>
2026-06-15 15:20:01 -0400

    Added urls.py and configured the path for the web app

--- mysite/web/views.py
+++ mysite/web/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+# Homepage 
+    # Display generic informaiton about the company and their services
+def index(request):
+    return render(request, 'homepage/index.html')
+    
