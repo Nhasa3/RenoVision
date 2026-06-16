842e85b14d1aa269c329effba6055803f614c88e
Nesar <nhasa0011@gmail.com>
2026-06-16 13:56:52 -0400

    updated views.py to have access to header file through homepage

--- mysite/web/views.py
+++ mysite/web/views.py
@@ -8,3 +8,13 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'homepage/index.html')
     
+    
+def about(request):
+    return render(request, 'About/about.html')
+
+def services(request):
+    return render(request, 'services/service.html')
+
+
+def basement(request):
+    return render(request, 'services/basement.html')
