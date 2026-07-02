708b9f9825d66034674d281cf6b3d73e34686d15
Nesar <nhasa0011@gmail.com>
2026-07-01 23:47:14 -0400

    Enabled built in admin dashboard from django and added customer model

--- mysite/web/admin.py
+++ mysite/web/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Customer, QuotePhoto
 
-# Register your models here.
+
+class QuotePhotoInline(admin.TabularInline):
+    model = QuotePhoto
+    extra = 1
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        "full_name",
+        "phone",
+        "email",
+        "project_type",
+        "budget",
+        "created_at",
+    )
+    search_fields = (
+        "full_name",
+        "email",
+        "phone",
+    )
+    list_filter = (
+        "project_type",
+        "budget",
+        "timeline",
+    )
+    inlines = [QuotePhotoInline]
