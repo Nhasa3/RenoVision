24b0fafa199b47ce76a2410ea1c43c3dca6ef370
Nesar <nhasa0011@gmail.com>
2026-07-10 14:53:13 -0400

    Added customer confirmation and admin notification email logic on email_services

---
+++ mysite/Email/Services/email_service.py
@@ -0,0 +1,40 @@
+
+
+
+
+def send_customer_confirmation(quote):
+    
+    subject = "Confirmation of your quote request"
+    
+    html_content = render_to_string("emails/customer_confirmation.html", {"quote": quote})
+    
+    email = EmailMultiAlternatives(
+        subject=subject,
+        body="Thank you for requesting a quote",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[quote.email],
+    )
+    
+    email.attach_alternative(html_content, "text/html")
+    email.send()
+    
+def send_admin_notification(quote):
+    subject = f"New Quote Request - {quote.full_name}"
+    
+    html_content = render_to_string(
+        "emails/admin_notification.html",
+        {"quote": quote},
+    )
+    
+    email = EmailMultiAlternatives(
+        subject=subject,
+        body="A new quote request has been submited.",
+        from_email=settings.DEFAULT_FROM_EMIAL,
+        to=[settings.ADMIN_EMAIL],
+        reply_to=[quote.email],
+    )
+    
+    email.attach_alternative(html_content, "text/html")
+    email.send()
+    
+    
