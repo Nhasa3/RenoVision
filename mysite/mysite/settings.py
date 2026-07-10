092575069bc3bcb90b78ff6f83cf2789b5c3fe98
Nesar <nhasa0011@gmail.com>
2026-07-10 14:40:45 -0400

    Configured settings for email feature and added files and folders to implement email on the system

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,3 +123,18 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Email Configuration
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.resend.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "resend"
+EMAIL_HOST_PASSWORD = env("RESEND_API_KEY")
+
+DEFAULT_FROM_EMAIL = "Ottawa Renovation <renotest93@gmail.com>"
+
+
